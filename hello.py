65f09e1f2b7802ecaa633ea37ed87a70caedbd09
uwuwuwuwuw <thangnguyhienthu69@gmail.com>
2019-06-07 21:47:10 +0700

    Them file hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("helloworld")
+import random
+m = random.randint(1,100)
+
+a = input("Insert N = ")
+n = int(a)
+
+if n < m:
+    print("N smaller than M")
+elif n > m:
+    print("N bigger than M")
+else:
+    print("You are lucky!")
